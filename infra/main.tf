9800d3463f9ecfbc7d62f8e8c2e9062a4313c701
ajamidar <jamidararnav@gmail.com>
2026-02-22 19:27:52 +0000

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -123,6 +123,13 @@ resource "aws_security_group" "ec2" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
